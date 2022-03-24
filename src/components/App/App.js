c2d8048423a476583498f821edecf35d6cc7916d
Eric Matlock <ermatlock@gmail.com>
2022-03-24 18:09:48 -0400

    Remove exemplar movie

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,17 +8,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-        "id": 694919,
-        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-        "title": "Money Plane",
-        "average_rating": 6.666666666666667,
-        "release_date": "2020-09-29"
-      },
-    ]
+      movies: []
     }
+  }
+
+  componentDidMount() {
+    this.setState()
   }
 
   render() {

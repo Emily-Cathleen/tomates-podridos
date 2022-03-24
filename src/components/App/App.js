4f00d84a8e8d81a19bf2ffe097af9ad3023a0766
Eric Matlock <ermatlock@gmail.com>
2022-03-24 17:05:33 -0400

    Add App component

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {
+        "id": 694919,
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "title": "Money Plane",
+        "average_rating": 6.666666666666667,
+        "release_date": "2020-09-29"
+      },
+    ]
+    }
+  }
+}

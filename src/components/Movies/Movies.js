791d8ebac8bb3bca39abdfd0bb7e6d8f00e64a4c
Eric Matlock <ermatlock@gmail.com>
2022-03-24 17:46:01 -0400

    Create initial movies functional component

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Movies.css"
+
+const Movies = () => {
+  return(
+    <div className="movies-section">
+      <h1>These are movies!</h1>
+    </div>
+
+  )
+}
+export default Movies

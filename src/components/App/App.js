06381dd433dfd5b7a644e8fc4193ce0e5d2ff4dc
Eric Matlock <ermatlock@gmail.com>
2022-03-24 17:20:20 -0400

    Add initial render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react"
 import './App.css';
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
 
 class App extends Component {
   constructor() {
@@ -17,4 +19,16 @@ class App extends Component {
     ]
     }
   }
+
+  render() {
+    return(
+      <main>
+        <Header />
+        <h1>These will be movies</h1>
+        <Footer />
+      </main>
+    )
+  } 
 }
+
+export default App

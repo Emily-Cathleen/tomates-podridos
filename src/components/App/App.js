2e8d3539f2ee50d6d435e1b328aaf414e6ce90b5
Eric Matlock <ermatlock@gmail.com>
2022-03-24 21:38:57 -0400

    Re-link file location

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react"
 import './App.css';
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
-import AllMovies from "../Movies/AllMovies"
+import AllMovies from "../AllMovies/AllMovies"
 import movieData from "../../movie-data";
 
 class App extends Component {

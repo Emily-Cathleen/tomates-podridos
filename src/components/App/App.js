e8b28b2f90388642ae0b5cf66d1c4b551a6f1a45
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-26 15:18:09 -0400

    Add function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import AllMovies from "../AllMovies/AllMovies"
-import movieData from "../../movie-data";
+import MovieModal from "../MovieModal/MovieModal"
+import {movieData} from "../../movie-data";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: []
+      movies: [],
+      selectedMovie: "",
+      isClicked: false,
     }
   }
 
@@ -17,15 +20,26 @@ class App extends Component {
     this.setState({ movies: movieData.movies })
   }
 
+  clickedMovie = (id) => {
+    console.log("CLICK")
+    const findSelectedMovie = this.state.movies.find(movie => {
+      return movie.id === id;
+    })
+    this.setState({selectedMovie: findSelectedMovie, isClicked: true})
+  }
+
+
+
   render() {
     return(
       <main>
         <Header />
-        <AllMovies movies={this.state.movies} />
+        <AllMovies movies={this.state.movies} clickedMovie={this.clickedMovie} />
+        {this.state.isClicked && <MovieModal movie={this.state.selectedMovie} />}
         <Footer />
       </main>
     )
-  } 
+  }
 }
 
 export default App

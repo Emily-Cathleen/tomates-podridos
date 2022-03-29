8fb4798dfc19987665a9b565fb71ca26b2b8419f
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-29 16:19:44 -0400

    Added Cypress test for footer

--- cypress/integration/main_page.js
+++ cypress/integration/main_page.js
@@ -1,5 +1,12 @@
 describe('Tomates podridos main page', () => {
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+  it('User should be able to visit the page and see title and all movies displayed', () => {
+    cy.visit('http://localhost:3000')
   });
+
+  it("Should have a footer with Company name", () => {
+    cy.visit('http://localhost:3000').contains('©2022 Tomates Podritos | A fake movie site by Emily C and Eric M')
+  });
+
+
+
 });

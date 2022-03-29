ed09d73d087d51bf6f278a759c382726b49c6e5b
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-29 15:42:19 -0400

    Import react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import AllMovies from "../AllMovies/AllMovies";
 import MovieModal from "../MovieModal/MovieModal";
 import ErrorModal from "../ErrorModal/ErrorModal"
 import { allMoviesData, singleMovieData } from "../../apiCalls";
+// import { } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -62,7 +63,7 @@ class App extends Component {
           />
         )}
         {this.state.hasError && (
-        <ErrorModal 
+        <ErrorModal
           error={this.state.error}
           closeModalButton={this.closeModalButton}
         />)}

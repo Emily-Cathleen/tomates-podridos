1418dd36a7f5cf31ac8bc04f62442cd4337d20d5
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-29 16:30:00 -0400

    Add describe block for Cypress testing to movie_page

--- cypress/integration/movie_page.js
+++ cypress/integration/movie_page.js
@@ -1,6 +1,10 @@
-// movie_page.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('Tomates podridos main page', () => {
+  it('User should be able to visit the page and see title and all movies displayed', () => {
+    cy.visit('http://localhost:3000')
+  });
+
+  it("Should have a footer with Company name", () => {
+    cy.visit('http://localhost:3000').contains('©2022 Tomates Podritos | A fake movie site by Emily C and Eric M')
+  });
+
+  });

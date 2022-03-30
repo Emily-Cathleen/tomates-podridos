c3a49b191a1452a6921f3a51b1237afef160b623
Eric Matlock <ermatlock@gmail.com>
2022-03-30 17:09:25 -0400

    Remove default link styling on text

--- src/components/AllMovies/AllMovies.js
+++ src/components/AllMovies/AllMovies.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const AllMovies = ({ movies, clickedMovie }) => {
   const movieCards = movies.map((movie) => {
     return (
-      <Link to={`/${movie.id}`} key={movie.id}>
+      <Link to={`/${movie.id}`} key={movie.id} style={{ textDecoration: 'none' }}>
       <MovieCard
         id={movie.id}
         image={movie.poster_path}

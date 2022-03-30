44d3b321b8510116169f278d29892d0f1ba17b3e
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-30 13:51:53 -0400

    Add Link to AllMovies

--- src/components/AllMovies/AllMovies.js
+++ src/components/AllMovies/AllMovies.js
@@ -6,10 +6,9 @@ import { Link } from 'react-router-dom';
 const AllMovies = ({ movies, clickedMovie }) => {
   const movieCards = movies.map((movie) => {
     return (
-      <Link to={`${movie.id}`} >
+      <Link to={`/${movie.id}`} key={movie.id}>
       <MovieCard
         id={movie.id}
-        key={movie.id}
         image={movie.poster_path}
         title={movie.title}
         clickedMovie={clickedMovie}

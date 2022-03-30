a935a98a44e5ef70affe0dc7193ab44288e2ba3c
Eric Matlock <ermatlock@gmail.com>
2022-03-30 18:45:36 -0400

    Adjust layout and add temp trailer

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,30 +1,40 @@
-import React from "react"
-import "./SingleMovie.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./SingleMovie.css";
+import { Link } from "react-router-dom";
 
 const SingleMovie = ({ selectedMovie, backButton }) => {
   return (
-    <div className="movie-modal">
+    <div
+      className="single-movie"
+      style={{ backgroundImage: `url(${selectedMovie.backdrop_path})` }}
+    >
+      {/* <img className="movie-bg" src={selectedMovie.backdrop_path} alt={selectedMovie.title}/> */}
       <div className="movie-trailer">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/aETz_dRDEys"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen="allowfullscreen"
+        ></iframe>
       </div>
       <section className="movie-details">
-      <img className="modal-img" src={selectedMovie.backdrop_path} alt={selectedMovie.title}/>
         <h1>{selectedMovie.title}</h1>
         <p>Released: {selectedMovie.release_date}</p>
         <p className="movie-description"> {selectedMovie.overview}</p>
         <p>{selectedMovie.genres}</p>
         <p>{selectedMovie.runtime} minutes</p>
         <p>{selectedMovie.average_rating}</p>
-        </section>
-        <div>
-        <Link to="/">
-          <button onClick={() => backButton()}>BACK</button>
-        </Link>
+        <div className="button-box">
+          <Link to="/">
+            <button onClick={() => backButton()}>BACK</button>
+          </Link>
         </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default SingleMovie
+export default SingleMovie;

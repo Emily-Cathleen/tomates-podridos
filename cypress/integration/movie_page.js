682df8697c01715ed81a62564ff5d477bd88fb0b
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-31 15:56:57 -0400

    Add cypress test for individula movie for the Youtube trailor link

--- cypress/integration/movie_page.js
+++ cypress/integration/movie_page.js
@@ -3,18 +3,27 @@ describe('Tomates podridos main page', () => {
     cy.visit('http://localhost:3000')
   });
 
+//HEADER
   it("Should have a header with Tomates Podriodos on it", () => {
     cy.visit('http://localhost:3000').contains("Tomates Podridos")
   });
 
+//FOOTER
   it("Should have a footer with Company name", () => {
     cy.visit('http://localhost:3000').contains('©2022 Tomates Podritos | A fake movie site by Emily C and Eric M')
   });
 
+//BACK BUTTON
   // it("Should display a button to return the user to home page", () => {
   //   cy.get(".x-button")
   //   .click()
   //   .url().should("eq", "http://localhost:3000/movie.id")
-  // })
+  // });
+
+//TRAILOR - YOUTUBE LINK
+  // it("Should display a trailor for the selected movie ", () => {
+  //   cy.visit('http://localhost:3000').contains('')
+  // });
+
 
   });

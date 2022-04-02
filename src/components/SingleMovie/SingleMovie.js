7d069272ceec6b13c401d1e3df3b55e1b9e3ddf1
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-02 16:50:46 -0400

    Refactored SingleMovie to become a class component

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import "./SingleMovie.css";
 import { Link } from "react-router-dom";
 import TomateMeter from "../TomateMeter/TomateMeter"
+import { getData, singleMovieData } from "../../apiCalls";
 
 
-const SingleMovie = ({ selectedMovie, backButton }) => {
+class SingleMovie extends Component {
+  constructor({ id }) {
+    super();
+    this.state = {
+      selectedMovie: {},
+      video: {}
+    }
+  }
+
+  componentDidMount = () => {
+    Promise.all ([
+    getData(id),
+    getData(`${id}/videos`)
+    ]).then((data) => {
+    this.setState({selectedMovie: data[0].movie, video: data[1].videos})
+  })
+
+  .catch((error) => this.throwError("Oops! something went wrong. Please try again. If problem persists, send complaints to Robbie and Scott"));
+  };
+
+
   return (
     <div
       className="single-movie"
       style={{ backgroundImage: `url(${selectedMovie.backdrop_path})` }}
     >
-      {/* <img className="movie-bg" src={selectedMovie.backdrop_path} alt={selectedMovie.title}/> */}
       <div className="movie-trailer">
         <iframe
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/aETz_dRDEys"
+          src={`https://www.youtube.com/embed/${video[0].key}`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -23,13 +43,13 @@ const SingleMovie = ({ selectedMovie, backButton }) => {
         ></iframe>
       </div>
       <section className="movie-details">
-        <h1>{selectedMovie.title}</h1>
-        <p>Released: {selectedMovie.release_date}</p>
-        <p className="movie-description"> {selectedMovie.overview}</p>
-        <p>{selectedMovie.genres}</p>
-        <p>{selectedMovie.runtime} minutes</p>
-        <p>{selectedMovie.average_rating}</p>
-        <TomateMeter rating={selectedMovie.average_rating}  />
+        <h1>{this.state.selectedMovie.title}</h1>
+        <p>Released: {this.state.selectedMovie.release_date}</p>
+        <p className="movie-description"> {this.state.selectedMovie.overview}</p>
+        <p>{this.state.selectedMovie.genres}</p>
+        <p>{this.state.selectedMovie.runtime} minutes</p>
+        <p>{this.state.selectedMovie.average_rating}</p>
+        <TomateMeter rating={this.state.selectedMovie.average_rating}  />
         <div className="button-box">
           <Link to="/">
             <button>BACK</button>

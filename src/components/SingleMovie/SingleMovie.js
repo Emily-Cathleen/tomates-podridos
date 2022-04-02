47f315de56982c2df348cc922549b5b3f85d428f
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-02 17:10:06 -0400

    Add src link bacm in. Still need to fix component.

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -37,6 +37,7 @@ class SingleMovie extends Component {
         <iframe
           width="100%"
           height="100%"
+          src={`https://www.youtube.com/embed/${video[0].key}`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

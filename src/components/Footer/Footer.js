1d84b088a66b1adce41964edb80ab95004714f1c
Eric Matlock <ermatlock@gmail.com>
2022-04-03 14:22:22 -0400

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
   return(
     <footer>
-      <p className="footer-text">©2022 Tomates Podritos</p>
+      <p className="footer-text">©2022 Tomates Podridos</p>
       <p className="footer-text">|</p>
       <p className="footer-text">A fake movie site by Emily C and Eric M</p>
     </footer>

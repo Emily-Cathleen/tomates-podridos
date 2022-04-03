6f406d5d2e72f7b173c5e10d27d92ab431471403
Eric Matlock <ermatlock@gmail.com>
2022-04-03 14:50:32 -0400

    Clean up comments and finalize testing

--- cypress/integration/movie_page.js
+++ cypress/integration/movie_page.js
@@ -13,32 +13,22 @@ describe("Tomates Podridos Movie Page", () => {
       });
   });
 
-  //FOOTER
-  it("Should have a footer with Company name", () => {
-    cy.visit("http://localhost:3000").contains(
-      "©2022 Tomates Podridos"
-    );
-  });
-
   //ERROR MSG
   it("Should display error message for 400 status code", () => {
     cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919", {
       statusCode: 404,
     });
-    cy.visit("http://localhost:3000/").contains(
+    cy.visit("http://localhost:3000/694919").contains(
       "Oops! something went wrong. Please try again. If problem persists, send complaints to Robbie and Scott"
     );
   });
 
-  //BACK BUTTON
-  // it("Should display a button to return the user to home page", () => {
-  //   cy.get(".x-button")
-  //   .click()
-  //   .url().should("eq", "http://localhost:3000/movie.id")
-  // });
+  // BACK BUTTON
+  it("Should display a button to return the user to home page", () => {
+    cy.visit("http://localhost:3000/694919")
+    cy.get('button').should('contain', 'BACK')
+    .click()
+    .url().should("eq", "http://localhost:3000/")
+  });
 
-  //TRAILOR - YOUTUBE LINK
-  // it("Should display a trailor for the selected movie ", () => {
-  //   cy.visit('http://localhost:3000').contains('')
-  // });
 });

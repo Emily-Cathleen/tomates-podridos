e01da6ecebb3a79200e5e6f9cf67b85df7f7f432
Eric Matlock <ermatlock@gmail.com>
2022-04-04 20:29:54 -0400

    Delete console logs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,9 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import AllMovies from "../AllMovies/AllMovies";
 import SingleMovie from "../SingleMovie/SingleMovie";
-import ErrorModal from "../ErrorModal/ErrorModal"
+import ErrorModal from "../ErrorModal/ErrorModal";
 import { getData } from "../../apiCalls";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -14,55 +14,63 @@ class App extends Component {
     this.state = {
       movies: [],
       error: "",
-      hasError: false
+      hasError: false,
     };
   }
 
   componentDidMount() {
-     getData("")
+    getData("")
       .then((data) => this.setState({ movies: data.movies }))
-      .catch((error) => this.throwError("Oops! something went wrong. Please try again. If problem persists, send complaints to Robbie and Scott"));
-  };
+      .catch((error) =>
+        this.throwError(
+          "Oops! something went wrong. Please try again. If problem persists, send complaints to Robbie and Scott"
+        )
+      );
+  }
 
   throwError = (error) => {
-    this.setState({ error: error})
+    this.setState({ error: error });
     this.setState({ hasError: true });
-  }
+  };
 
   closeModalButton = () => {
     this.setState({ hasError: false });
   };
 
   clickedMovie = (id) => {
-    console.log("thisis the id", id)
-    this.setState({selectedMovieId: id});
-
-};
-
+    this.setState({ selectedMovieId: id });
+  };
 
   render() {
     return (
       <main className="fade-in">
         <Header />
-          <Switch>
+        <Switch>
           {/* HomePage */}
-            <Route exact path="/" render={() =>
-            <AllMovies movies={this.state.movies} clickedMovie={this.clickedMovie}/> }
-            />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <AllMovies
+                movies={this.state.movies}
+                clickedMovie={this.clickedMovie}
+              />
+            )}
+          />
           {/* SingleMoviePage */}
-            <Route path="/:id" render={({match}) => {
-              return <SingleMovie id={match.params.id} /> 
-              }}
-            />
-            {/* 404 Page */}
-            
-
-          </Switch>
+          <Route
+            path="/:id"
+            render={({ match }) => {
+              return <SingleMovie id={match.params.id} />;
+            }}
+          />
+        </Switch>
         {this.state.hasError && (
-        <ErrorModal
-          error={this.state.error}
-          closeModalButton={this.closeModalButton}
-        />)}
+          <ErrorModal
+            error={this.state.error}
+            closeModalButton={this.closeModalButton}
+          />
+        )}
 
         <Footer />
       </main>
